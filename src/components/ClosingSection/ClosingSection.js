23163b2d244f01bcd761a267149161bfee2a0601
Julia Breeden <juliabreeden1018@gmail.com>
2024-01-27 19:50:39 -0600

    finishing touches pt2

--- src/components/ClosingSection/ClosingSection.js
+++ src/components/ClosingSection/ClosingSection.js
@@ -4,7 +4,7 @@ const ClosingSection = () => {
     return (
         <div className="closing-section">
             <h2>I'm Ready to Join Pinterest!</h2>
-            <p>Thank you for considering my application. I'm enthusiastic about the opportunity to bring my skills to the Pinterest team and challenge myself in a world-class engineering environment. My love for continuous learning, technical knowledge, and my mission to live by our shared values of inspiration and positive action convince me I am ready to meaningfully contribute as a Pinterest Engineering Apprentice.</p>
+            <p>I hope I've portrayed my enthusiasm about the opportunity to bring my skills to the team and challenge myself in a world-class engineering environment. My love for continuous learning, technical knowledge, and my mission to live by our shared values of inspiration and positive action convince me I am ready to meaningfully contribute as a Pinterest Apprentice.</p>
             <p style={{fontWeight: 'bold'}}> I'm excited about the possibility of contributing to the happiest corner of the internet and bringing imagination to people's lives.</p>
             <p>Thank you so much, and feel free to reach me at juliabreeden1018@gmail.com.</p>
         </div>
