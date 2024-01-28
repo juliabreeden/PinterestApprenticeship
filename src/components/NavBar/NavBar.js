a406d57d66146dfcfe29d053ec57e52080c6b314
Julia Breeden <juliabreeden1018@gmail.com>
2024-01-28 14:07:36 -0600

    nav bar order corrected

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -15,8 +15,8 @@ const NavBar = () => {
             </div>
             <div className={isNavOpen ? "nav-links show" : "nav-links"}>
                 <a href="#aboutMe" onClick={() => setIsNavOpen(false)}>About Me</a>
-                <a href="#why-me" onClick={() => setIsNavOpen(false)}>Why Me?</a>
                 <a href="#projects" onClick={() => setIsNavOpen(false)}>Projects</a>
+                <a href="#why-me" onClick={() => setIsNavOpen(false)}>Why Me?</a>
                 <a href="https://www.linkedin.com/in/julia-breeden-0726b692/">LinkedIn</a>
                 <a href="https://github.com/juliabreeden">GitHub</a>
             </div>
